9e2700659d73b461a0e67da0a34b8a5329a455b4
João Vitor Araujo <joao18araujo@gmail.com>
2017-06-06 10:05:47 -0300

    Add d-pad to JoystickConfigState and d-pad sprites

--- src/JoystickConfigState.cpp
+++ src/JoystickConfigState.cpp
@@ -5,19 +5,29 @@
 #include "JoystickButton.h"
 #include "Game.h"
 
-#define A_X 800
-#define A_Y 500
-#define LB_X A_X - 500
-#define LB_Y A_Y - 300
-#define SELECT_X A_X - 300
-#define SELECT_Y A_Y - 200
+#define MIDDLE_X 550
+#define BOTTOM_Y 500
+#define A_X MIDDLE_X + 250
+#define A_Y BOTTOM_Y
+#define LB_X MIDDLE_X - 250
+#define LB_Y BOTTOM_Y - 300
+#define DOWN_X LB_X
+#define DOWN_Y BOTTOM_Y - 25
+#define SELECT_X MIDDLE_X - 75
+#define SELECT_Y BOTTOM_Y - 200
 
 JoystickConfigState::JoystickConfigState(int joystick_id){
+	//D-Pad
+	add_object(new JoystickButton(DOWN_X, DOWN_Y - 100, "UP", 0, joystick_id, "up"));
+	add_object(new JoystickButton(DOWN_X + 50, DOWN_Y - 50, "RIGHT", 1, joystick_id, "right"));
+	add_object(new JoystickButton(DOWN_X, DOWN_Y, "DOWN", 2, joystick_id, "down"));
+	add_object(new JoystickButton(DOWN_X - 50, DOWN_Y - 50, "LEFT", 3, joystick_id, "left"));
+
 	//ABXY
+	add_object(new JoystickButton(A_X - 75, A_Y - 75, "X", 4, joystick_id, "button"));
 	add_object(new JoystickButton(A_X, A_Y, "A", 5, joystick_id, "button"));
-	add_object(new JoystickButton(A_X + 100, A_Y - 100, "B", 6, joystick_id, "button"));
-	add_object(new JoystickButton(A_X, A_Y - 200, "Y", 7, joystick_id, "button"));
-	add_object(new JoystickButton(A_X - 100, A_Y - 100, "X", 4, joystick_id, "button"));
+	add_object(new JoystickButton(A_X + 75, A_Y - 75, "B", 6, joystick_id, "button"));
+	add_object(new JoystickButton(A_X, A_Y - 150, "Y", 7, joystick_id, "button"));
 
 	//Triggers
 	add_object(new JoystickButton(LB_X, LB_Y, "LB", 8, joystick_id, "trigger"));
@@ -27,9 +37,7 @@ JoystickConfigState::JoystickConfigState(int joystick_id){
 
 	//Select e start
 	add_object(new JoystickButton(SELECT_X, SELECT_Y, "select", 12, joystick_id, "select_start"));
-	add_object(new JoystickButton(SELECT_X + 150, SELECT_Y, "start", 13, joystick_id, "select_start"));
-
-
+	add_object(new JoystickButton(MIDDLE_X + 75, SELECT_Y, "start", 13, joystick_id, "select_start"));
 }
 
 void JoystickConfigState::update(float delta){
