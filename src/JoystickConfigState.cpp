02a04701a06abc7263543c9066ee922142553ed9
João Vitor Araujo <joao18araujo@gmail.com>
2017-06-06 09:12:30 -0300

    Create first button pressed feedback

--- src/JoystickConfigState.cpp
+++ src/JoystickConfigState.cpp
@@ -1,14 +1,33 @@
 #include "JoystickConfigState.h"
 
-JoystickConfigState::JoystickConfigState(int joystick_id){
+#include "InputManager.h"
+#include "MenuState.h"
+#include "JoystickButton.h"
+#include "Game.h"
 
+JoystickConfigState::JoystickConfigState(int joystick_id){
+	add_object(new JoystickButton(100, 100, "A", 5, joystick_id, "button"));
 }
 
 void JoystickConfigState::update(float delta){
+	InputManager * inputManager = InputManager::get_instance();
+
+	if(inputManager->key_press(SDLK_ESCAPE)){
+		m_quit_requested = true;
+		Game::get_instance().push(new MenuState());
+		return;
+	}
+
+	if(inputManager->quit_requested()){
+		m_quit_requested = true;
+		return;
+	}
+
+	update_array(delta);
 }
 
 void JoystickConfigState::render(){
-
+	render_array();
 }
 
 void JoystickConfigState::pause(){
