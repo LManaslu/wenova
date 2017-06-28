6f974cedc7e567012ddc9f804da55355d222af56
Vitor Barbosa <vitornga15@gmail.com>
2017-06-27 23:29:41 -0300

    Add skins mechanism

--- include/CharacterSelectState.h
+++ include/CharacterSelectState.h
@@ -13,13 +13,13 @@ class CharacterSelectState : public State {
 private:
 	Sprite background, character_slots;
 	Sprite name_tag[4], number[4];
-	int cur_selection_row[4], cur_selection_col[4];
+	int cur_selection_row[4], cur_selection_col[4], cur_skin[4];
 	vector<ii> name_tag_positions, number_delta, name_delta, sprite_pos;
 	vector<int> col_slots, row_slots;
 	vector< vector<string> > names;
 
 	map<string, Sprite> char_name;
-	unordered_map<string, Sprite> char_sprite;
+	unordered_map<string, vector<Sprite> > char_sprite;
 
 public:
 	CharacterSelectState();
@@ -31,6 +31,7 @@ public:
 	void resume();
 
 	bool character_enabled(int row, int col);
+	string get_char_name(int idx);
 };
 
 #endif
