095448991aad4fa89bc96b89067edb3a0de4436b
Vitor Barbosa <vitornga15@gmail.com>
2017-06-28 00:03:18 -0300

    Add way to prevent chosing duplicate skins
    Also, enable start when all players select a character

--- include/CharacterSelectState.h
+++ include/CharacterSelectState.h
@@ -13,13 +13,17 @@ class CharacterSelectState : public State {
 private:
 	Sprite background, character_slots;
 	Sprite name_tag[4], number[4];
-	int cur_selection_row[4], cur_selection_col[4], cur_skin[4];
+	int cur_selection_row[4], cur_selection_col[4];
 	vector<ii> name_tag_positions, number_delta, name_delta, sprite_pos;
 	vector<int> col_slots, row_slots;
 	vector< vector<string> > names;
 
 	map<string, Sprite> char_name;
 	unordered_map<string, vector<Sprite> > char_sprite;
+
+	int cur_skin[4];
+	map<string, vector<bool> > available_skin;
+	bool selected[4];
 
 public:
 	CharacterSelectState();
@@ -32,6 +36,7 @@ public:
 
 	bool character_enabled(int row, int col);
 	string get_char_name(int idx);
+	bool all_players_selected();
 };
 
 #endif
