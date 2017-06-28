592b8a084b6051a60b85e115feb096521bbdb07d
Vitor Barbosa <vitornga15@gmail.com>
2017-06-27 23:03:23 -0300

    Add character preview in selection menu

--- include/CharacterSelectState.h
+++ include/CharacterSelectState.h
@@ -13,11 +13,13 @@ class CharacterSelectState : public State {
 private:
 	Sprite background, character_slots;
 	Sprite name_tag[4], number[4];
-	map<string, Sprite> char_name;
 	int cur_selection_row[4], cur_selection_col[4];
-	vector<ii> name_tag_positions, number_delta, name_delta;
+	vector<ii> name_tag_positions, number_delta, name_delta, sprite_pos;
 	vector<int> col_slots, row_slots;
 	vector< vector<string> > names;
+
+	map<string, Sprite> char_name;
+	unordered_map<string, Sprite> char_sprite;
 
 public:
 	CharacterSelectState();
