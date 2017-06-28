bc6eb8689db4afe27d121b756ef69793012ab271
Vitor Barbosa <vitornga15@gmail.com>
2017-06-27 22:13:56 -0300

    Add character selection movement

--- include/CharacterSelectState.h
+++ include/CharacterSelectState.h
@@ -12,9 +12,10 @@ using std::to_string;
 class CharacterSelectState : public State {
 private:
 	Sprite background, character_slots;
-	Sprite name_tag[4];
-	int cur_selection[4];
-	vector<ii> name_tag_positions;
+	Sprite name_tag[4], number[4];
+	int cur_selection_row[4], cur_selection_col[4];
+	vector<ii> name_tag_positions, number_delta;
+	vector<int> col_slots, row_slots;
 
 public:
 	CharacterSelectState();
@@ -24,6 +25,8 @@ public:
 
 	void pause();
 	void resume();
+
+	bool enabled(int row, int col);
 };
 
 #endif
