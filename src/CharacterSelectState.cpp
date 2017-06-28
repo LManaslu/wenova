8dfc6dce92ad6f9e1da310c301009cb158772df1
Vitor Barbosa <vitornga15@gmail.com>
2017-06-27 22:41:36 -0300

    Add character names in character selection menu

--- src/CharacterSelectState.cpp
+++ src/CharacterSelectState.cpp
@@ -7,11 +7,15 @@ CharacterSelectState::CharacterSelectState(){
 	background = Sprite("character_select/background.png");
 	character_slots = Sprite("character_select/character_slots.png");
 
+	char_name["blood"] = Sprite("character_select/name_blood.png");
+	char_name["flesh"] = Sprite("character_select/name_flesh.png");
+
 	for(int i=0;i<4;i++){
 		name_tag[i] = Sprite("character_select/name_tag_" + to_string(i + 1) + ".png");
 		number[i] = Sprite("character_select/number_" + to_string(i + 1) + ".png");
 	}
 
+	names = { {"blood",""}, {"flesh",""}, {"",""}, {"",""} };
 
 	memset(cur_selection_col, 0, sizeof cur_selection_col);
 	memset(cur_selection_row, 0, sizeof cur_selection_row);
@@ -22,6 +26,7 @@ CharacterSelectState::CharacterSelectState(){
 	row_slots = { 55, 197, 395, 536 };
 
 	number_delta = { ii(12, 9), ii(93, 9), ii(12, 101), ii(93, 101) };
+	name_delta = { ii(118, 53), ii(117, 55), ii(47, 54), ii(50, 54) };
 }
 
 void CharacterSelectState::update(float delta){
@@ -42,22 +47,22 @@ void CharacterSelectState::update(float delta){
 
 	for(int i=0;i<4;i++){
 		if((input_manager->key_press(SDLK_LEFT) || input_manager->joystick_button_press(InputManager::LEFT, i)) && cur_selection_col[i] != 0){
-			if(enabled(cur_selection_row[i], cur_selection_col[i] - 1))
+			if(character_enabled(cur_selection_row[i], cur_selection_col[i] - 1))
 			cur_selection_col[i]--;
 		}
 
 		if((input_manager->key_press(SDLK_RIGHT) || input_manager->joystick_button_press(InputManager::RIGHT, i)) && cur_selection_col[i] != 1){
-			if(enabled(cur_selection_row[i], cur_selection_col[i] + 1))
+			if(character_enabled(cur_selection_row[i], cur_selection_col[i] + 1))
 			cur_selection_col[i]++;
 		}
 
 		if(input_manager->key_press(SDLK_UP) && cur_selection_row[i] != 0){
-			if(enabled(cur_selection_row[i] - 1, cur_selection_col[i]))
+			if(character_enabled(cur_selection_row[i] - 1, cur_selection_col[i]))
 			cur_selection_row[i]--;
 		}
 
 		if(input_manager->key_press(SDLK_DOWN) && cur_selection_row[i] != 3){
-			if(enabled(cur_selection_row[i] + 1, cur_selection_col[i]))
+			if(character_enabled(cur_selection_row[i] + 1, cur_selection_col[i]))
 			cur_selection_row[i]++;
 		}
 	}
@@ -69,14 +74,18 @@ void CharacterSelectState::render(){
 	character_slots.render(0, 0);
 
 	for(int i=0;i<4;i++){
+		name_tag[i].render(name_tag_positions[i].first, name_tag_positions[i].second);
+
 		int col_sel = cur_selection_col[i];
 		int row_sel = cur_selection_row[i];
 
-		number[i].render(col_slots[col_sel] + number_delta[i].first, row_slots[row_sel] + number_delta[i].second);
-	}
+		string char_selected = names[col_sel][row_sel];
 
-	for(int i=0;i<4;i++){
-		name_tag[i].render(name_tag_positions[i].first, name_tag_positions[i].second);
+		char_name[char_selected].render(
+			name_tag_positions[i].first + name_delta[i].first,
+			name_tag_positions[i].second + name_delta[i].second
+		);
+		number[i].render(col_slots[col_sel] + number_delta[i].first, row_slots[row_sel] + number_delta[i].second);
 	}
 }
 
@@ -88,7 +97,7 @@ void CharacterSelectState::resume(){
 
 }
 
-bool CharacterSelectState::enabled(int row, int){
+bool CharacterSelectState::character_enabled(int row, int){
 	// Only characters in first row are available
 	return row == 0;
 }
