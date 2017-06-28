894c054e60ef5f9caf6ee23e5d91841a728d4229
Vitor Barbosa <vitornga15@gmail.com>
2017-06-28 17:06:01 -0300

    Change background and character select sprites

--- include/CharacterSelectState.h
+++ include/CharacterSelectState.h
@@ -11,7 +11,7 @@ using std::to_string;
 
 class CharacterSelectState : public State {
 private:
-	Sprite background, character_slots;
+	Sprite background[2], planet, character_slots;
 	Sprite name_tag[4], number[4], selected_tag;
 	int cur_selection_row[4], cur_selection_col[4];
 	vector<ii> name_tag_positions, number_delta, name_delta, sprite_pos;
