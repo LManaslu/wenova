5c8f8af28e60f90f1d104c4dda9f26cd47404db4
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-06-30 11:43:28 -0300

    Rename methods

--- include/Flesh.h
+++ include/Flesh.h
@@ -8,22 +8,22 @@ class Flesh : public Fighter{
 		Flesh(string skin, float x, float y, int cid, Fighter *cpartner = nullptr);
 
 	private:
-		void jump(bool change = true);
-		void fall(bool change = true);
-		void left(bool change = true);
-		void right(bool change = true);
-		void idle(bool change = true);
-		void crouch(bool change = true);
-		void idle_atk_neutral(bool change = true);
-		void idle_atk_neutral_1(bool change = true);
-		void idle_atk_neutral_2(bool change = true);
-		void idle_atk_neutral_3(bool change = true);
-		void idle_atk_front(bool change = true);
-		void jump_atk_down_fallloop(bool change = true);
-		void jump_atk_down_dmg(bool change = true);
-		void idle_atk_down(bool change = true);
+		void check_jump(bool change = true);
+		void check_fall(bool change = true);
+		void check_left(bool change = true);
+		void check_right(bool change = true);
+		void check_idle(bool change = true);
+		void check_crouch(bool change = true);
+		void check_idle_atk_neutral(bool change = true);
+		void check_idle_atk_neutral_1(bool change = true);
+		void check_idle_atk_neutral_2(bool change = true);
+		void check_idle_atk_neutral_3(bool change = true);
+		void check_idle_atk_front(bool change = true);
+		void check_jump_atk_down_fallloop(bool change = true);
+		void check_jump_atk_down_dmg(bool change = true);
+		void check_idle_atk_down(bool change = true);
 
-		void update_machine_state();
+		void update_machine_state(float delta);
 };
 
 #endif
