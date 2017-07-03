e6ed831544f34b63555dc97741db3a58113b6fae
João Paulo Busche <joaopaulo.busche@gmail.com>
2017-07-03 14:12:47 -0300

    Add process input to stage selection

--- src/StageSelectStage.cpp
+++ src/StageSelectStage.cpp
@@ -22,6 +22,7 @@ StageSelectState::StageSelectState() {
 }
 
 void StageSelectState::update(float delta) {
+	process_input();
 	InputManager * input_manager = InputManager::get_instance();
 
 	if(input_manager->quit_requested()){
@@ -29,14 +30,14 @@ void StageSelectState::update(float delta) {
 		return;
 	}
 
-	if(input_manager->key_press(InputManager::K_LEFT)) {
+	if(pressed[LEFT_BUTTON]) {
 		update_stage_select(-1);
 	}
-	if(input_manager->key_press(InputManager::K_RIGHT)) {
+	if(pressed[RIGHT_BUTTON]) {
 		update_stage_select(1);
 	}
 
-	if(input_manager->key_press(InputManager::K_A)) {
+	if(pressed[SELECT_BUTTON]) {
 		m_quit_requested = true;
 		if(stage_select == 0){
 			srand(clock());
@@ -45,7 +46,7 @@ void StageSelectState::update(float delta) {
 		Game::get_instance().push(new CharacterSelectState(to_string(stage_select)));
 	}
 
-	if(input_manager->key_press(InputManager::K_B)) {
+	if(pressed[BACK_BUTTON]) {
 		m_quit_requested = true;
 		Game::get_instance().push(new MenuState(true));
 		return;
@@ -76,4 +77,36 @@ void StageSelectState::update_stage_select(int increment) {
 	stage_select += increment;
 	if(stage_select < 0) stage_select = 0;
 	if(stage_select > N_STAGES - 1) stage_select = N_STAGES - 1;
+}
+
+void StageSelectState::process_input(){
+	InputManager * input_manager = InputManager::get_instance();
+
+	vector< pair<int, int> > buttons = {
+		ii(LEFT_BUTTON, InputManager::K_LEFT),
+		ii(RIGHT_BUTTON, InputManager::K_RIGHT),
+		ii(SELECT_BUTTON, InputManager::K_A),
+		ii(BACK_BUTTON, InputManager::K_B)
+	};
+
+	vector< pair<int, int> > joystick_buttons = {
+		ii(LEFT_BUTTON, InputManager::LEFT),
+		ii(RIGHT_BUTTON, InputManager::RIGHT),
+		ii(SELECT_BUTTON, InputManager::A),
+		ii(BACK_BUTTON, InputManager::B)
+	};
+
+	if(SDL_NumJoysticks() != 0){
+		for(ii button : joystick_buttons){
+			pressed[button.first] = input_manager->joystick_button_press(button.second, 0);
+			is_holding[button.first] = input_manager->is_joystick_button_down(button.second, 0);
+			released[button.first] = input_manager->joystick_button_release(button.second, 0);
+		}
+	}else{
+		for(ii button : buttons){
+			pressed[button.first] = input_manager->key_press(button.second, true);
+			is_holding[button.first] = input_manager->is_key_down(button.second, true);
+			released[button.first] = input_manager->key_release(button.second, true);
+		}
+	}
 }
