787e385822e727815dbdf5a791172bb96b57b264
Vitor Barbosa <vitornga15@gmail.com>
2017-07-06 23:32:05 -0300

    Add keyboard help in options

--- include/JoystickConfigState.h
+++ include/JoystickConfigState.h
@@ -9,13 +9,13 @@
 
 class JoystickConfigState : public State{
 private:
-	Sprite background, joystick_help, back_btn, test_btn;
+	Sprite background, joystick_help, back_btn, test_btn, keyboard;
 	Text *back_txt, *test_txt, *press, *select_text, *and_text, *start_text, *to_go_back;
-	bool on_test;
+	bool on_test, is_keyboard;
 	Sound blocked, selected, changed;
 
 public:
-	JoystickConfigState(int joystick_id);
+	JoystickConfigState(int joystick_id, bool ckeyboard = false);
 
 	void update(float delta);
 	void render();
