2866ebcc789426f7ebe9c28cfb5d0bc2c7dc31be
Rudmar <rudmar_junior@hotmail.com>
2017-08-28 12:57:39 -0300

    Applying stylesheet

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -11,18 +11,18 @@ float Camera::layer_speed[LAYERS] = {4, 6, 16, 32};
 #define SCREEN_HEIGHT 600
 //FIXME layer virar variável
 
-void Camera::follow(GameObject * new_focus){
+void Camera::follow(GameObject * new_focus) {
 	focus = new_focus;
 }
 
-void Camera::unfollow(){
+void Camera::unfollow() {
 	focus = nullptr;
 }
 
-void Camera::update(float delta){
+void Camera::update(float delta) {
 	InputManager * input_manager = InputManager::get_instance();
 
-	if(focus == nullptr){
+	if (focus == nullptr) {
 		bool directions[] = {
 			input_manager->is_joystick_button_down(0, InputManager::LEFT),
 			input_manager->is_joystick_button_down(0, InputManager::UP),
@@ -32,19 +32,21 @@ void Camera::update(float delta){
 
 		speed.x = 0;
 		speed.y = 0;
-		for(int i = 0; i < 4; ++i){
+		for (int i = 0; i < 4; ++i) {
 			speed.x += ((i+1) % 2) * (i - 1) * directions[i] * delta;
 			speed.y += (i % 2) * (i - 2) * directions[i] * delta;
 		}
 
-		for(int i = 0; i < LAYERS; ++i){
+		for (int i = 0; i < LAYERS; ++i) {
 			pos[i].x += speed.x * layer_speed[i];
 			pos[i].y += speed.y * layer_speed[i];
 		}
-	}else{
-		for(int i = 0; i < LAYERS; ++i){
-			pos[i].x = (-focus->box.get_x() + SCREEN_WIDTH/2.0) * layer_speed[i]/layer_speed[PENGUIN_LAYER];
-			pos[i].y = (-focus->box.get_y() + SCREEN_HEIGHT/2.0) * layer_speed[i]/layer_speed[PENGUIN_LAYER];
+	} else {
+		for (int i = 0; i < LAYERS; ++i) {
+			pos[i].x = (-focus->box.get_x() + SCREEN_WIDTH/2.0) *
+			layer_speed[i]/layer_speed[PENGUIN_LAYER];
+			pos[i].y = (-focus->box.get_y() + SCREEN_HEIGHT/2.0) *
+			layer_speed[i]/layer_speed[PENGUIN_LAYER];
 		}
 	}
 }
